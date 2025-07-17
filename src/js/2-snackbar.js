5a569c85185777f382d91ff61f5afa4dab489b94
Yehor <qugar04@gmail.com>
2025-07-17 04:36:55 +0300

    fixed

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = Number(e.target.elements.delay.value);
+  const state = e.target.elements.state.value;
+
+  createPromise(delay, state).then(showSucces).catch(showError);
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+function showSucces(delay) {
+  iziToast.success({
+    title: 'OK',
+    message: `✅ Fulfilled promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
+
+function showError(delay) {
+  iziToast.error({
+    title: 'Error',
+    message: `❌ Rejected promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
+
+formEl.addEventListener('submit', onSubmit);
